814f2656a75bbd1f6aa7701a9911f4cd3e09df17
glizondo <80534057+glizondo@users.noreply.github.com>
2023-02-23 20:13:42 -0500

    Added methods for Scoring class

--- src/Scoring.java
+++ src/Scoring.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class Scoring implements Subject {
 	int scoreLocal;
@@ -7,7 +8,6 @@ public class Scoring implements Subject {
 	ArrayList<Observer> observerList;
 
 	public Scoring() {
-		super();
 		observerList = new ArrayList<Observer>();
 
 	}
@@ -24,10 +24,24 @@ public class Scoring implements Subject {
 
 	@Override
 	public void notifyObservers() {
-		if (isQuarter) {
-
+		for (Iterator<Observer> it = observerList.iterator(); it.hasNext();) {
+			Observer o = it.next();
+			o.update(scoreLocal, scoreVisitant, isQuarter);
 		}
+	}
 
+	private int getLocalScore() {
+		return (int) Math.floor(Math.random() * 33);
+	}
+
+	private int getVisitantScore() {
+		return (int) Math.floor(Math.random() * 33);
+	}
+
+	public void dataChanged() {
+		scoreLocal += getLocalScore();
+		scoreVisitant += getVisitantScore();
+		notifyObservers();
 	}
 
 }
