e26d9d901b30aa0001e9846cffd9e2e1e570cafe
glizondo <80534057+glizondo@users.noreply.github.com>
2023-02-23 19:21:24 -0500

    Added structure main game

--- src/Driver.java
+++ src/Driver.java
@@ -3,30 +3,29 @@ import java.util.Scanner;
 public class Driver {
 
 	static Scanner keyboard;
-	static boolean wantsToKeepPlaying;
 
 	public static void main(String[] args) {
 		ChoicesUser();
-		
-		
-		
+
 	}
 
 	private static void ChoicesUser() {
 		keyboard = new Scanner(System.in);
+		int answer = 0;
+		
+		while (answer != 8) {
+			
+			System.out.println("Press 1 --- New Game");
+			System.out.println("Press 2 --- Simulate one quarter");
+			System.out.println("Press 3 --- Print current score");
+			System.out.println("Press 4 --- Print current prediction");
+			System.out.println("Press 5 --- Print prediction stats");
+			System.out.println("Press 6 --- Print table results finished games");
+			System.out.println("Press 7 --- Print new piece title with results");
+			System.out.println("Press 8 --- Quit");
 
-		System.out.println("Press 1 --- New Game");
-		System.out.println("Press 2 --- Simulate one quarter");
-		System.out.println("Press 3 --- Print current score");
-		System.out.println("Press 4 --- Print current prediction");
-		System.out.println("Press 5 --- Print prediction stats");
-		System.out.println("Press 6 --- Print table results finished games");
+			answer = keyboard.nextInt();
 
-
-
-		int answer = keyboard.nextInt();
-
-		while (answer != 4) {
 			if (answer == 1) {
 
 			}
@@ -34,6 +33,18 @@ public class Driver {
 
 			}
 			if (answer == 3) {
+
+			}
+			if (answer == 4) {
+
+			}
+			if (answer == 5) {
+
+			}
+			if (answer == 6) {
+
+			}
+			if (answer == 7) {
 
 			}
 
